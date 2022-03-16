d679f6ffc44e8ead57812e054fdce24acb91063c
Nityasmit Mallick <mallicknityasmit2001@gmail.com>
2022-03-17 01:14:35 +0530

    CRUD Operations in JSON Server

--- src/App.js
+++ src/App.js
@@ -25,24 +25,61 @@ function App() {
     return data
   }
 
-  // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 100000) + 1
-    const newTask = { id, ...task }
+  // Get All Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
 
-    console.log(newTask)
+    return data
+  }
+
+  // Add Task
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    })
+
+    const newTask = await res.json()
+
     setTasks([...tasks, newTask])
+
+    // const id = Math.floor(Math.random() * 100000) + 1
+    // const newTask = { id, ...task }
+
+    // console.log(newTask)
+    // setTasks([...tasks, newTask])
   }
 
   // Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    })
+
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
   // Toggle Remainder
-  const toggleReminder = (id) => {
-    // console.log(id)
-    setTasks(tasks.map((task) => (task.id === id ? { ...task, reminder: !task.reminder } : task)))
+  const toggleReminder = async (id) => {
+    const task = await fetchTask(id)
+    task.reminder = !task.reminder
+    // const updatedTask = { ...task, reminder: !task.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    })
+
+    const data = await res.json()
+
+    setTasks(tasks.map((task) => (task.id === id ? { ...task, reminder: data.reminder } : task)))
   }
 
   return (
